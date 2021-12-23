955da9fc670c4f4f0e95c4db0b0e56702bf1e654
장효원 <hyo961015@naver.com>
2021-12-23 22:33:43 +0900

    ADD ActionSheet

--- UIAlertController_Examples/ViewController.swift
+++ UIAlertController_Examples/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
     
-    let data:[String] = ["OK Alert", "OK And Cancel Alert", "ALL Alert"]
+    let data:[String] = ["OK Alert", "OK And Cancel Alert", "ALL Alert", "OK ActionSheet", "OK And Cancel ActionSheet", "ALL ActionSheet"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,6 +65,53 @@ class ViewController: UIViewController {
         self.present(alertVC, animated: true, completion: nil)
     }
     
+    func showOKActionSheet() {
+        let alertVC = UIAlertController(title: "title", message: "PreferredStyle is ActionSheet", preferredStyle: .actionSheet)
+        let successAction = UIAlertAction(title: "OK", style: .default){ _ in
+            print("success")
+        }
+        
+        alertVC.addAction(successAction)
+        
+        self.present(alertVC, animated: true, completion: nil)
+    }
+    
+    func showOKAndCancelActionSheet() {
+        let alertVC = UIAlertController(title: "title", message: "PreferredStyle is ActionSheet", preferredStyle: .actionSheet)
+        
+        let successAction = UIAlertAction(title: "OK", style: .default) { _ in
+            print("success")
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            print("cancel")
+        }
+        
+        alertVC.addAction(successAction)
+        alertVC.addAction(cancelAction)
+        
+        self.present(alertVC, animated: true, completion: nil)
+    }
+    
+    func showALLActionSheet() {
+        let alertVC = UIAlertController(title: "title", message: "PreferredStyle is ActionSheet", preferredStyle: .actionSheet)
+        
+        let successAction = UIAlertAction(title: "OK", style: .default) { _ in
+            print("success")
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            print("cancel")
+        }
+        let destructiveAction = UIAlertAction(title: "Destructive", style: .destructive) { _ in
+            print("destructive")
+        }
+        
+        alertVC.addAction(successAction)
+        alertVC.addAction(cancelAction)
+        alertVC.addAction(destructiveAction)
+        
+        self.present(alertVC, animated: true, completion: nil)
+    }
+    
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
@@ -76,6 +123,12 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
             showOKAndCancelAlert()
         case 2:
             showALLAlert()
+        case 3:
+            showOKActionSheet()
+        case 4:
+            showOKAndCancelActionSheet()
+        case 5:
+            showALLActionSheet()
         default:
             showOKAndCancelAlert()
         }
